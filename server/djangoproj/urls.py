63a964dcbf8117b96788edff473b7856968bcb98
Sreeharivs1983 <sreeharivs69@gmail.com>
2026-03-01 01:12:31 -0500

    Module 4 completed: Added Dealers list page, Dealer details with dynamic reviews, integrated sentiment microservice, implemented Post Review functionality, fixed routing issues, and completed frontend-backend integration

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -12,7 +12,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-
+    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
     # Home Page
     path('', TemplateView.as_view(template_name="Home.html")),
 
